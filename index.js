929028e0e97e8145be4a797e44c7330ab086d3fc
Mashoeu MM <mashoeumm@gmail.com>
2022-05-19 20:18:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+ //
+ const name = "Matthews";
+ const surname= 'Mashoeu';
+ const contact = '0719206023';
+ const email='mashoeumm@gmail.com';
+ const residential="Stand 10128 Mochadi GlenCowie";
+ const postaladdress="P O Box 364 Glen 1061";
+ const institution="Richfieald Institute Of Graduates";
+ const qualification="Info Tech & Computer Science";
+ 
+//
+
+let personalinfo=[
+    'Matthews', 
+    'Mashoeu',
+    'From Glen Cowie',
+    'To do'
+];
+personalinfo[5]=12
+console.log(personalinfo);
+
+function welcomeMessage(){
+    console.log('Welcome To Matthews Mashoeu Webpage For CodeTribe');
+}
+  welcomeMessage();
+
+function square(number){
+    return number * number;
+}
+console.log(square(3));
+
+let x = 20;
+let y = 23;
+
+ //console.log(x + y);
+ console.log(x >= y );
+
+
+
+
